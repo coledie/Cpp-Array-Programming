330867ad6185789a176d1ca06c672624f8490ca4
csdhv9 <csdhv9@umsystem.edu>
2020-12-11 03:23:57 -0600

    Add empty creation routine.

--- src/creation.cpp
+++ src/creation.cpp
@@ -15,13 +15,13 @@ namespace nd {
     template <typename T>
     T* _full(const int& dims, const int*& shape, const T& fill_value){
         int size = get_size(dims, shape);
-        return _full(size, fill_value);
+        return _full<T>(size, fill_value);
     }
 
 
     template <typename T>
     ndarray<T> zeros(const int& size){
-        T* data = _full(size, 0);
+        T* data = _full<T>(size, 0);
         ndarray<T> output(size, data);
         return output;
     }
@@ -29,7 +29,7 @@ namespace nd {
 
     template <typename T>
     ndarray<T> zeros(const int& dims, const int*& shape){
-        T* data = _full(dims, shape, 0);
+        T* data = _full<T>(dims, shape, 0);
         ndarray<T> output(dims, shape, data);
         return output;
     }
@@ -37,7 +37,7 @@ namespace nd {
 
     template <typename T>
     ndarray<T> ones(const int& size){
-        T* data = _full(size, 1);
+        T* data = _full<T>(size, 1);
         ndarray<T> output(size, data);
         return output;
     }
@@ -45,8 +45,23 @@ namespace nd {
 
     template <typename T>
     ndarray<T> ones(const int& dims, const int*& shape){
-        T* data = _full(dims, shape, 1);
+        T* data = _full<T>(dims, shape, 1);
         ndarray<T> output(dims, shape, data);
         return output;
+    }
+
+
+    template <typename T>
+    ndarray<T> empty(const int& size){
+        T* data = new T[size];
+        ndarray<T> output(size, data);
+        return output;
+    }
+
+
+    template <typename T>
+    ndarray<T> empty(const int& dims, const int*& shape){
+        int size = get_size(dims, shape);
+        return empty<T>(size);
     }
 }
