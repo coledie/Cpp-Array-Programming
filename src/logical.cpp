2d5c0b006e2489952f5f3a27d441a9ef5a6f2d0d
csdhv9 <csdhv9@umsystem.edu>
2020-12-11 03:35:13 -0600

    Add max, min, amax, amin.

--- src/logical.cpp
+++ src/logical.cpp
@@ -1,8 +1,60 @@
 #include "../src/nd.h"
 #include <set>
+#include <limits>
 
 
 namespace nd {
+    template <typename T>
+    bool _min_operator(T& a, T& b){ return a < b; }
+    template <typename T>
+    bool _max_operator(T& a, T& b){ return a > b; }
+
+
+    template <typename T, typename C>
+    T compare_value(ndarray<T>& input){
+        T value_min = std::numeric_limits<T>::min(), value_max = std::numeric_limits<T>::max();
+        T value, value_curr = (C(value_min, value_max) ? value_max : value_min);
+
+        for(int i=0; i < input.size(); i++){
+            value = input.data()[i];
+            if (value < value_curr)
+                value_curr = value;
+        }
+
+        return value_curr;
+    }
+
+
+    template <typename T>
+    T min(ndarray<T>& input){ return compare_value<T, _min_operator>(input); }
+    template <typename T>
+    T max(ndarray<T>& input){ return compare_value<T, _max_operator>(input); }
+
+
+    template <typename T, typename C>
+    int compare_idx(ndarray<T>& input){
+        T value_min = std::numeric_limits<T>::min(), value_max = std::numeric_limits<T>::max();
+        T value, value_curr = (C(value_min, value_max) ? value_max : value_min);
+        int idx_curr = -1;
+
+        for(int i=0; i < input.size(); i++){
+            value = input.data()[i];
+            if (value < value_curr){
+                value_curr = value;
+                idx_curr = i;
+            }
+        }
+
+        return idx_curr;
+    }
+
+
+    template <typename T>
+    int amin(ndarray<T>& input){ return compare_idx<T, _min_operator>(input); }
+    template <typename T>
+    int amax(ndarray<T>& input){ return compare_idx<T, _max_operator>(input); }
+
+
     template <typename T>
     ndarray<T> unique(ndarray<T>& input){
         /* Return ndarray of unique items. */
