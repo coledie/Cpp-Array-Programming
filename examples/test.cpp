c94d261947b1d3b2325afabf9beeabbed8b07ebf
csdhv9 <csdhv9@umsystem.edu>
2020-12-12 23:51:58 -0600

    Extend tests.

--- examples/test.cpp
+++ examples/test.cpp
@@ -19,6 +19,7 @@ int main() {
    assert(nd::min(data) == 0);
    assert(nd::amax(data) == size-1);
    assert(nd::amin(data) == 0);
+   assert(data[3] == 3);
 
    std::cout << "Shape: [";
    for(int i=0; i < data.dims(); i++)
@@ -43,8 +44,38 @@ int main() {
    assert(data.front() == 0);
    assert(data.back() == size-1);
 
-   ndarray<dtype> data_new = nd::copy(data);
-   // EDIT check see if affects other
+   int new_value = 10;
+   int pos_idx_edit[] = {1, 1};
+   printf("%i -> ", data[pos_idx_edit]);
+   data[pos_idx_edit] = new_value;
+   printf("%i\n", data[pos_idx_edit]);
+   assert(data[pos_idx_edit] == new_value);
+
+   ndarray<dtype> data_copy = nd::copy(data);
+   for(int i=0; i<data.size(); ++i)
+      assert(data[i] == data_copy[i]);
+
+   int pos_slice_a[] = {0, 0}, pos_slice_b[] = {2, 2};
+   ndarray<dtype> slice = data_copy(pos_slice_a, pos_slice_b);
+   for(int i=0; i<slice.size(); ++i)
+      assert(slice[i] == data_copy[i]);
+
+   for(int i=0; i<slice.size(); ++i)
+      slice.data()[i] = i+1;
+   for(int i=0; i<slice.size(); ++i)
+      assert(slice[i] == data_copy[i]);
+   for(int i=0; i<slice.size(); ++i)
+      assert(slice[i] != data[i]);
+
+   int size_new_copy = 6;
+   ndarray<dtype> data_copy2 = nd::resize(data_copy, size_new_copy);
+   assert(data_copy2.size() == size_new_copy);
+   for(int i=0; i<slice.size(); ++i)
+      assert(slice[i] == data_copy2[i]);
+   data_copy.~ndarray();
+   for(int i=0; i<slice.size(); ++i){
+      assert(i+1 == data_copy2[i]);
+   }
 
    return 0;
 }
